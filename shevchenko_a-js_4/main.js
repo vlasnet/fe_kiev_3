12f29dd1df4ba9fd564cdb0cc0d3ccfb54b56b2e
Alexey Shevchenko <shevchenkoalexeymail@gmail.com>
2017-09-15 20:47:47 +0300

    added js_4

---
+++ shevchenko_a-js_4/main.js
@@ -0,0 +1,210 @@
+'use strict'
+
+function clickControl(control, action) {
+	control.addEventListener('click', action);
+}
+
+// taskOne
+
+function doRed() {
+	let taskOne = document.querySelectorAll('.task-one p');
+	for (let i = 0; i < taskOne.length; i++) {
+		taskOne[i].style.backgroundColor = "#f00";
+	}
+}
+
+doRed();
+
+// TaskTwo
+
+function doShadow() {
+	let taskTwo = document.querySelectorAll('img');
+	for (var i = 0; i < taskTwo.length; i++) {
+		taskTwo[i].style.boxShadow = "5px 5px 1px #000";
+	}
+}
+
+doShadow();
+
+// TaskThree
+
+function addNumber() {
+	let taskThree = document.querySelectorAll('.task-three p');
+	for (var i = 0; i < taskThree.length; i++) {
+		taskThree[i].innerText = (i + 1) + ". " + taskThree[i].innerText;
+	}
+}
+
+addNumber();
+
+// TaskFour
+
+let taskFour = {
+	btnOne: document.querySelector('.task-four__btn-one'),
+	btnTwo: document.querySelector('.task-four__btn-two'),
+	clickResult: document.querySelector('.task-four__result'),
+	action: function () {
+		taskFour.clickResult.innerText = (+taskFour.clickResult.innerText + 1);
+	}
+}
+
+clickControl(taskFour.btnOne, taskFour.action);
+clickControl(taskFour.btnTwo, taskFour.action);
+
+// taskFive
+
+let taskFive = {
+	inputOne: document.querySelector('#num'),
+	inputTwo: document.querySelector('#pow'),
+	btn: document.querySelector('.btn_pow'),
+	action: function () {
+		let result = powNum(taskFive.inputOne.value, taskFive.inputTwo.value);
+		alert(result)
+	}
+};
+
+function powNum(num, pow) {
+	let result = num;
+	if (pow === 0) {
+		return 1;
+	}
+
+	else if (pow < 1) {
+		return 'Введите число больше 0';
+	}
+
+	else if (pow === 1) {
+		return num;
+	}
+
+	else {
+		for (var i = 2; i <= pow ; i++) {
+			result *= num;
+		}
+		return result;
+	}
+}
+
+clickControl(taskFive.btn, taskFive.action);
+
+// TaskSix
+
+let taskSix = {
+	inputOne: document.querySelector('#tagName'),
+	btn: document.querySelector('.btn_use-underline'),
+	action: function () {
+		let someElements = document.querySelectorAll(taskSix.inputOne.value);
+		console.log(someElements);
+		for (var i = 0; i < someElements.length; i++) {
+			someElements[i].style.textDecoration = 'underline';
+		}
+	}
+}
+
+clickControl(taskSix.btn, taskSix.action);
+
+
+// TaskSeven and TaskEight
+
+let taskSeven = {
+	input: document.querySelector('#task7'),
+	btn: document.querySelector('.task-seven__btn'),
+	result: document.querySelector('.task-seven__result'),
+	action: function() {
+		if (taskSeven.input.value > 16) {
+			taskSeven.result.innerText = 'Welcom';
+		}
+
+		else if (taskSeven.input.value == undefined || (!taskSeven.input.value)) {
+			taskSeven.result.innerText = 'Enter your age';
+		}
+
+		else if (taskSeven.input.value <= 16) {
+			taskSeven.result.innerText = 'You are still young';
+		}
+
+	}
+}
+
+clickControl(taskSeven.btn, taskSeven.action);
+
+
+// TaskNine 
+
+let someArr = [1,2,3,4];
+
+function arrLength(arr) {
+	if (arr) {
+		return arr.length;
+	}
+
+	else {
+		return 'Error';
+	}
+}
+
+console.log(arrLength(someArr));
+console.log(arrLength());
+
+// TaskTen
+
+let taskTen = {
+	input: document.querySelector('#task10'),
+	btn: document.querySelector('.task-ten__btn'),
+	result: document.querySelector('.task-ten__result'),
+	action: function() {
+		taskTen.result.innerText = returnResult();
+	}
+}
+
+function returnResult() {
+	if (taskTen.input.value > 10) {
+		return  Math.sqrt(taskTen.input.value);
+	}
+
+	else if (taskTen.input.value < 7) {
+		return 'Your number < 7';
+	}
+
+	else if (+taskTen.input.value === 8 || +taskTen.input.value === 9) {
+		return taskTen.input.value - 1;
+	}
+}
+
+clickControl(taskTen.btn, taskTen.action);
+
+// TaskEleven
+
+let taskEleven = {
+	randomNum: Math.round(Math.random() * 10),
+	howManyAttempts: 0,
+	inputOne: document.querySelector('#task11'),
+	btn: document.querySelector('.task-eleven__btn'),
+	result: document.querySelector('.task-eleven__result'),
+	action: function() {
+		
+		if (taskEleven.inputOne.value == taskEleven.randomNum) {
+			taskEleven.howManyAttempts++;
+			taskEleven.result.innerText = 'Well done! You are try ' + taskEleven.howManyAttempts + ' times';
+		}
+
+		else if (taskEleven.howManyAttempts >= 3) {
+			taskEleven.result.innerText = 'Sorry, but you lose :(';
+		}
+
+		else if (taskEleven.inputOne.value > taskEleven.randomNum) {
+			taskEleven.result.innerText = 'Need Less';
+			taskEleven.howManyAttempts++;
+		}
+
+		else if (taskEleven.inputOne.value < taskEleven.randomNum) {
+			taskEleven.result.innerText = 'Need more';
+			taskEleven.howManyAttempts++;
+		}
+
+	}
+}
+//для проверки какое число выпало =)
+console.log(taskEleven.randomNum);
+clickControl(taskEleven.btn, taskEleven.action);
+
