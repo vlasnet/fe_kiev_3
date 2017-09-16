9b05195b15da7a476ae54e88d1cfb5e8debb940b
Oksana Lesnik <oksana.lesnik.v@gmail.com>
2017-09-16 10:39:25 +0300

    initial commit

---
+++ Lesnik_O_js_js-2/main.js
@@ -0,0 +1,14 @@
+
+// Task1
+
+var YearBirth = prompt('Укажите Ваш год рождения в формате 1988 или 02?', 100);
+
+if (YearBirth.length =2)
+
+{
+	Yearbirth=20+Yearbirth;
+}
+
+alert('Вам ' + years + ' лет!')
+
+
---
+++ Lesnik_O_js_js-2/offlinetask.js
@@ -0,0 +1,10 @@
+
+var fruits = ['apple', 'orange', 'palm'];
+
+
+for (let i=0; i<3; i++) {
+	console.log(fruits(i));
+}
+
+
+
