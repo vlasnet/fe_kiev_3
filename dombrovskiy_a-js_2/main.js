c35aeb25d50b2713f7ab9282d8490f662f1c02bf
Aleksey Dombrovskiy <realkawul@gmail.com>
2017-09-24 20:08:22 +0300

    modified

--- dombrovskiy_a-js_2/main.js
+++ dombrovskiy_a-js_2/main.js
@@ -1,64 +1,66 @@
-function yearsOld() {
+function getYear() {
   'use strict';
 
   var start = document.getElementById("yearsOld").value;
-  var parag = document.getElementById("task_1");
+  var span = document.getElementById("task_1");
   var date = new Date();
-  var answer =  date.getFullYear() - start;
+  var answer = date.getFullYear() - start;
 
-  parag.innerHTML = answer;
+  span.innerHTML = answer;
 }
 
-function biggerNumb() {
+function getBiggerNumber() {
   'use strict';
 
-  var start = document.getElementById("firstYears").value;
-  var end = document.getElementById("lastYears").value;
+  var firstNumber = document.getElementById("firstNumber").value;
+  var secondNumber = document.getElementById("secondNumber").value;
   var answer = document.getElementById('task_2');
 
-if (start > end ) {
-  answer.innerHTML = start;
-} else if (end > start) {
-  answer.innerHTML = end;
-} else {
-  answer.innerHTML = "Числа равны";
-}
+  if (firstNumber > secondNumber) {
+    answer.innerHTML = firstNumber;
+  } else if (secondNumber > firstNumber) {
+    answer.innerHTML = secondNumber;
+  } else {
+    answer.innerHTML = "Числа равны";
+  }
 }
 
-function entrance() {
+function getEntrance() {
   'use strict';
 
   var apartments = document.getElementById('number').value;
   var answer = document.getElementById('task_3');
 
-    if (apartments > 0 && apartments <= 20) {
-  		answer.innerHTML = "1";
-  	} else if (apartments >= 21 && apartments <= 64) {
-  		answer.innerHTML = "2";
-  	} else if (apartments >= 65 && apartments <= 80) {
-  		answer.innerHTML = "3";
-  	}
+  if (apartments > 0 && apartments <= 20) {
+    answer.innerHTML = "1";
+  } else if (apartments >= 21 && apartments <= 64) {
+    answer.innerHTML = "2";
+  } else if (apartments >= 65 && apartments <= 80) {
+    answer.innerHTML = "3";
+  } else {
+    answer.innerHTML = "Такого подьезда нет";
+  }
 }
 
-function logIn() {
+function getAccount() {
   'use strict';
 
   var login = document.getElementById('login').value;
   var pass = document.getElementById('password').value;
   var answer = document.getElementById('task_4');
 
-  if ( login == 'ivan' && pass == 333 ) {
-      answer.innerHTML = "Hello ivan";
-    } else if (login == 'ssss' && pass == 666 ) {
-      answer.innerHTML = "Hello ssss";
-    } else if (login == 'gibs' && pass == '0000' ) {
-      answer.innerHTML = "Hello gibs";
+  if (login == 'ivan' && pass == 333) {
+    answer.innerHTML = "Hello ivan";
+  } else if (login == 'ssss' && pass == 666) {
+    answer.innerHTML = "Hello ssss";
+  } else if (login == 'gibs' && pass == '0000') {
+    answer.innerHTML = "Hello gibs";
   } else {
-      answer.innerHTML = "Error";
+    answer.innerHTML = "Error";
   }
 }
 
-function comparison(){
+function getNumber() {
   'use strict';
 
   var first = document.getElementById('first').value;
@@ -66,100 +68,110 @@ function comparison(){
   var third = document.getElementById('third').value;
   var answer = document.getElementById('task_5');
 
-  if(first > second){
-    if(first > third){
+  if (first > second) {
+    if (first > third) {
       answer.innerHTML = first;
     }
-  } else if(second > third){
+  } else if (second > third) {
     answer.innerHTML = second;
-  } else {
+  }else{
     answer.innerHTML = third;
-}
+  }
 }
 
-function half() {
-    'use strict';
+function getPairedNumbers() {
+  'use strict';
 
   var answer = document.getElementById('task_for_1');
-  for(var i = 0; i <= 100; i++ ) {
-    if(i%2==0){
-      answer.innerHTML = answer.innerHTML + i + ",";
+  for (var i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+      answer.innerHTML += i + ",";
     }
   }
 }
 
-function twoHundred() {
-    'use strict';
+function getTwoHundred() {
+  'use strict';
 
   var answer = document.getElementById('task_for_2');
-  for (var i = 200; i >= 0; i--){
+  for (var i = 200; i >= 0; i--) {
     answer.innerHTML += i + ",";
   }
 }
 
-function sum() {
-    'use strict';
+function getAmount() {
+  'use strict';
 
   var answer = document.getElementById('task_for_3');
-	var sum = 0;
-	for (var i = 1; i <= 100; i++) {
-		 sum = sum + i; }
-	 answer.innerHTML = sum;
+  var amount = 0;
+  for (var i = 1; i <= 100; i++) {
+    amount += i;
+  }
+  answer.innerHTML = amount;
 }
 
-function pow() {
+function getPower() {
   'use strict';
 
   var answer = document.getElementById('task_for_4');
-  var num = document.getElementById('num').value;
-  var exp = document.getElementById('exponent').value;
+  var number = document.getElementById('numbers').value;
+  var exponent = document.getElementById('exponent').value;
+  var value = number;
 
-  if (exp > 0) {
-    var val = num;
-    for (var i = 1; i < exp; i++ ) {
-      val *= num;
+  if (exponent > 0) {
+    for (var i = 0; i < exponent; i++) {
+      value *= number;
     }
-  	answer.innerHTML = val;
-	} else if ( exp < 0 ) {
-  	var val = num;
-    exp = (-1) * exp;
-    for ( var i = 1; i < exp; i++ ) {
-      val *= num;
+    answer.innerHTML = value;
+
+  } else if (exponent < 0) {
+
+    exponent = (-1) * exponent;
+    for (var k = 1; k < exponent; k++) {
+      value *= number;
     }
-  	answer.innerHTML = (1 / val);
-	} else {
-  	answer.innerHTML = 1;
-	}
+    answer.innerHTML = (1 / value);
+
+  } else {
+    answer.innerHTML = 1;
+
+  }
 }
 
-function table() {
+function getTable() {
   'use strict';
 
   var answer = document.getElementById('task_for_5');
   var number = document.getElementById('numberForTable').value;
 
-  for (var i = 1; i < 10; i++){
-    var ended = i * number;
-    answer.innerHTML += (i + " * " + number + " = " + ended + '<br>');
+  for (var i = 1; i < 10; i++) {
+    var table = i * number;
+    answer.innerHTML += (i + " * " + number + " = " + table + '<br>');
   }
 }
 
-function multiplication() {
-    'use strict';
+function getMultiplication() {
+  'use strict';
 
   var answer = document.getElementById('task_for_6');
-	var sum = 1;
-	for (var i = 1; i <= 50; i++) {
-		 sum = sum * i; }
-	 answer.innerHTML = sum;
+  var amount = 1;
+  for (var i = 1; i <= 50; i++) {
+    amount *= i;
+  }
+  answer.innerHTML = amount;
 }
 
-function symbol() {
+function getSymbol() {
   'use strict';
 
   var answer = document.getElementById('task_for_7');
 
-  for(var i = 1000; i <=2000; i++){
+  for (var i = 1000; i <= 2000; i++) {
     answer.innerHTML += ('&#' + i + ' ');
   }
 }
+
+var a = {};
+var b = {};
+
+console.log(a = b);
