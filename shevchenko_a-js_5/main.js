11109534076d8bad466af6466346ba468d850671
AlexeyShevchenkoUN <shevchenkoalexeymail@gmail.com>
2017-09-28 18:47:29 +0300

    FIX bugs

--- shevchenko_a-js_5/main.js
+++ shevchenko_a-js_5/main.js
@@ -1,13 +1,12 @@
 'use strict'
 
-// Как обычно, для отслеживания кликов =)
 
 function clickControl(control, action) {
 	control.addEventListener('click', action);
 }
 
 
-// Задание 1
+// Task 1
 
 let taskOne = {
 	input:  document.querySelector('.taskOne__input'),
@@ -20,7 +19,7 @@ let taskOne = {
 
 clickControl(taskOne.btn, taskOne.action);
 
-// Задание 2
+// Task 2
 
 let taskTwo = {
 	imgs:   document.querySelectorAll('.taskTwo__img'),
@@ -33,39 +32,22 @@ let taskTwo = {
 
 taskTwo.action();
 
-// Задание 3 и 4 (зачем делать двойную работу? =) )
+// Task 3 and Task 4
 
 let taskThree = {
 	input:  document.querySelector('.taskThree__input'),
 	btn:    document.querySelector('.taskThree__btn'),
 	result: document.querySelector('.taskThree__result'),
 	action: function () {
-		if (~taskThree.input.value.indexOf('http://www.')) {
-			taskThree.result.innerText = taskThree.input.value.slice(11);
-		}
-
-		else if (~taskThree.input.value.indexOf('https://www.')) {
-			taskThree.result.innerText = taskThree.input.value.slice(12);
-		}
-
-		else if (~taskThree.input.value.indexOf('http://')) {
-			taskThree.result.innerText = taskThree.input.value.slice(7);
-		}
-
-		else if (~taskThree.input.value.indexOf('https://')) {
-			taskThree.result.innerText = taskThree.input.value.slice(8);
-		}
-
-		else {
-			taskThree.result.innerText = taskThree.input.value;
-
-		}
+		let url = document.createElement('a');
+		url.href = taskThree.input.value;
+		taskThree.result.innerText = url.hostname.replace(/^www\./,"");
 	}
 };
 
 clickControl(taskThree.btn, taskThree.action);
 
-// Задание 5
+// Task 5
 
 let taskFiveForm = document.taskFive;
 
@@ -75,44 +57,20 @@ let taskFive = {
 	btn: taskFiveForm.submit,
 	result: document.querySelector('.taskFive__result'),
 	action: function() {
-		let a = taskFiveForm;
 		for (var i = 0; i < taskFiveForm.length - 1; i++) {
-			 let a = taskFiveForm[i].value
-			if (a) {
-				// демонстрация того, что пробелы удалены
-				user[i] = removeSpaces(a.split('')).join('');
-			}
-			else return false;
+			let a = taskFiveForm[i].name;
+			user[a] = taskFiveForm[i].value.trim();
 		}
 
-		if ((user[2].length > 5) && (~user[3].indexOf('@')) && (!(~user[3].indexOf(' ')))) {
+		if ((user.pass.length > 5) && (~user.email.indexOf('@')) && (!(~user.email.indexOf(' ')))) {
 			return true;
 		}
 		else return false;
 	}
 };
 
-function removeSpaces(arr) {
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[0] == ' ') {
-			arr.shift();
-
-		}
-	}
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[arr.length - 1] == ' ') {
-			arr.pop();
-		}
-	}
-	return arr;
-}
-
 clickControl(taskFive.btn, function () {
 	taskFive.result.innerText = taskFive.action();		
-
-	// демонстрация того, что пробелы удалены
-	console.log(user);
-
 });
 
 // Задание 6
