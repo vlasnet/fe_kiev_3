c1bd75e638d53e6cceb7d40e96f5c4fe98c64206
Miniaylo.S <po4tovuj@gmail.com>
2017-10-19 15:30:45 +0300

    finish task-3

--- miniajlo_s-js_6/main.js
+++ miniajlo_s-js_6/main.js
@@ -1,21 +1,21 @@
 'use strict';
 // task-1
-(function () {
-let result = document.querySelector('.task_1-result');
-let elem = document.getElementById("container");
-function onMousemove(event) {
-    let x = event.offsetX;
-    let y = event.offsetY;
-    result.textContent = "X Position: " + x + ", Y Position: " + y;
-};
+(function() {
+    let result = document.querySelector('.task_1-result');
+    let elem = document.getElementById("container");
+    function onMousemove(event) {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        result.textContent = "X Position: " + x + ", Y Position: " + y;
+    };
 
-elem.addEventListener("mousemove", onMousemove, false);
+    elem.addEventListener("mousemove", onMousemove, false);
 })();
 
 // task-2
-(function () {
+(function() {
     let elem = document.querySelector('.folder');
-    elem.addEventListener('dblclick', function () {
+    elem.addEventListener('dblclick', function() {
         if (elem.classList.contains('closed')) {
             elem.classList.toggle('closed');
             elem.classList.toggle('open');
@@ -28,8 +28,26 @@ elem.addEventListener("mousemove", onMousemove, false);
 })();
 
 // task-3
-(function () {
-    let div = document.querySelector('.container');
-    
+(function() {
+    let divContainer = document.querySelector('.container');
+    for (let i = 0; i < 350; i++) {
+        let div = '<div class="box">' +
+        ' ' +
+        '</div>';
+        divContainer.insertAdjacentHTML('beforeEnd', div);
+        // Или так:
+        // let div = document.createElement('div');
+        // div.classList.add('box');
+        // divContainer.appendChild(div);
+    };
+    // function() {
+        let box = document.querySelectorAll('.box');
+        for (let i = 0; i < box.length; i++) {
+            box[i].addEventListener('mouseover', function() {
+                box[i].style.borderRadius = '50%';
+            });
+         }
+
+    // };
 
 })();
