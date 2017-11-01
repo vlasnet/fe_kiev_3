c08360488f27a3379500d605fb01e34a603c7b3c
Miniaylo.S <po4tovuj@gmail.com>
2017-11-01 15:18:48 +0200

    try to group console.log

--- miniajlo_s-js_m-1/main.js
+++ miniajlo_s-js_m-1/main.js
@@ -7,8 +7,22 @@ const stringOne = 'qwertyuiop[]\\',
     stringThreeLength = stringThree.length,
     position1 = stringOne.indexOf('['),
     position2 = stringOne.indexOf(']');
-console.log(`stringOne length = ${stringOneLength}, stringTwo length = ${stringTwoLength}, stringThree length = ${stringThreeLength}`);
-console.log(`First symbol: stringOne = ${stringOne.charAt(0)}, stringTwo = ${stringTwo.charAt(0)}, stringThree = ${stringThree.charAt(0)} `);
-console.log(`Last symbol: stringOne = ${stringOne.charAt(12)}, stringTwo = ${stringTwo.charAt(10)}, stringThree = ${stringThree.charAt(9)}`);
-console.log(`Symbols position: Symbol '[' is on ${position1} position,
-Symbol ']' is on ${position2} position`);
+console.group('Strings length:')
+console.log (`first string length = ${stringOneLength}`);
+console.log (`second string length = ${stringTwoLength}`);
+console.log (`third string length = ${stringThreeLength}`);
+console.groupEnd();
+console.group('First symbol in strings:');
+console.log (`First symbol in first string is: ${stringOne.charAt(0)}`);
+console.log (`First symbol in second string is: ${stringTwo.charAt(0)}`);
+console.log (`First symbol in third string is: ${stringThree.charAt(0)}`);
+console.groupEnd();
+console.group('Last symbol in strings:')
+console.log (`Last symbol in first string is: ${stringOne.charAt(12)}`);
+console.log (`Last symbol in second string is: ${stringTwo.charAt(10)}`);
+console.log (`Last symbol in third string is: ${stringThree.charAt(9)}`);
+console.groupEnd();
+console.group('Symbols position:');
+console.log (`Symbol '[' is on ${position1} position`);
+console.log (`Symbol ']' is on ${position2} position`);
+console.groupEnd();
