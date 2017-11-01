67c55fcbceb2e3209448ed2dcf960c54b78a7ffa
Miniaylo.S <po4tovuj@gmail.com>
2017-11-01 14:47:27 +0200

    fix

--- miniajlo_s-js_m-1/main.js
+++ miniajlo_s-js_m-1/main.js
@@ -12,39 +12,3 @@ console.log(`First symbol: stringOne = ${stringOne.charAt(0)}, stringTwo = ${str
 console.log(`Last symbol: stringOne = ${stringOne.charAt(12)}, stringTwo = ${stringTwo.charAt(10)}, stringThree = ${stringThree.charAt(9)}`);
 console.log(`Symbols position: Symbol '[' is on ${position1} position,
 Symbol ']' is on ${position2} position`);
-
-// module 2
-(function() {
-    let sharm = 15,
-        hurgada = 25,
-        taba = 6,
-        amount = document.querySelector('.vocation');
-    amount.addEventListener('click', function() {
-        let number = prompt('Введите количество отдыхающих', '');
-        if (number.value <= hurgada) {
-            let confirm = confirm('Есть места в гуппе hurgada, Вы подтверждаете поездку?');
-            if (confirm = true) {
-                hurgada = hurgada - number.value;
-                alert('Хорошо Вам отдохнуть!');
-            } else if (number.value <= sharm) {
-                confirm('Есть места в гуппе sharm, Вы подтверждаете поездку?');
-                if (confirm == true) {
-                    sharm = sharm - number.value;
-                    alert('Хорошо Вам отдохнуть!');
-                } else if (number.value <= taba) {
-                    confirm('Есть места в гуппе taba, Вы подтверждаете поездку?');
-                    if (confirm == true) {
-                        taba = taba - number.value;
-                        alert('Хорошо Вам отдохнуть!');
-                    } else {
-                        alert('Мест нет');
-                    }
-                } else {
-                    alert('Мест нет');
-                }
-            }
-        } else {
-            alert('У нас нет такого количества мест!')
-        }
-    })
-})();
