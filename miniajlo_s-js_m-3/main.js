506723ef40721691b86d8748fdb3e29717b96bf0
Miniaylo.S <po4tovuj@gmail.com>
2017-11-02 16:08:39 +0200

    finish module3

--- miniajlo_s-js_m-3/main.js
+++ miniajlo_s-js_m-3/main.js
@@ -1,7 +1,21 @@
 'use strict';
-let alphabet = [
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', ],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\''],
-    ['z', 'x', 'c', 'v', 'b', 'n', 'm', '\,', '\.', '/']
-];
-let first = al
+const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+let keyboard = [];
+keyboard.push(alphabet.slice(0, 12));
+keyboard.push(alphabet.slice(12, 23));
+keyboard.push(alphabet.slice(23, ));
+console.log (keyboard);
+console.log (`Вторая строка клавиатуры: ${keyboard[1]}`);
+
+let hello = keyboard[1][5] + keyboard[0][2] + keyboard[1][8] + keyboard[1][8] +
+keyboard[0][8];
+console.log(hello);
+
+let javascript = keyboard[1][6] + keyboard[1][0] + keyboard[2][3] + keyboard[1][0] +
+keyboard[1][1] + keyboard[2][2] + keyboard[0][3] + keyboard[0][7] + keyboard[0][9] +
+keyboard[0][4];
+console.log(javascript);
+
+let trainer = keyboard[0][4] + keyboard[0][3] + keyboard[1][0] + keyboard[0][7] +
+keyboard[2][5] + keyboard[0][2] + keyboard[0][3];
+console.log(trainer);
