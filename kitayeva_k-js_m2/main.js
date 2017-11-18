51a6be3c62330bc4dc04fc45383b1ab9df2eac7a
Kseniya Kitayeva <kseniyakit@gmail.com>
2017-11-18 22:07:33 +0200

    fix js_m2

--- kitayeva_k-js_m2/main.js
+++ kitayeva_k-js_m2/main.js
@@ -4,22 +4,24 @@ let freePlaceCount = {
     Tuba: 6
 };
 function checkPlace() {
-    let places = parseInt(prompt('Введите количество человек: ', ''));
-    if (isNaN(places) || places < 0 ) {
-        alert('Введите еще раз');
-    } else if (places > 25) {
-        alert('Мест нет');
-    } else
-        for (let key in freePlaceCount) {
-            if (places <= freePlaceCount[key]) {
-                let result = confirm(`Есть место в ${key}, Вам подходит?`);
-                if (result == true) {
-                    freePlaceCount[key] = freePlaceCount[key] - places;
-                    console.log(`${freePlaceCount[key]} мест`);
-                    alert(`Спасибо, вы выбрали ${key}`);
-                    break;
-                } else alert(`К сожалению в ${key} мест нет`);
+    let places = +(prompt('Введите количество человек: ', ''));
+    let i = 1;
+    for (let key in freePlaceCount) {
+        i++;
+        if (places <= freePlaceCount[key] && Number.isInteger(places)) {
+            if (confirm(`Есть место в ${key}, Вам подходит?`)) {
+                freePlaceCount[key] = freePlaceCount[key] - places;
+                console.log(`В ${key} осталось ${freePlaces[key]} мест`);
+                alert(`Спасибо, вы выбрали ${key}`);
+                break;
+            } else if (i >= Object.keys(places).length) {
+                alert(`К сожалению в ${key} мест нет`);
+
+            }
+            else {
+                alert('Попробуйте еще раз');
             }
         }
+    }
 }
 document.getElementById('chooseCity').addEventListener('click',checkPlace);
