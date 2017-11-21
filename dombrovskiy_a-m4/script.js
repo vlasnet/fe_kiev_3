435c58ddc46528b34560b6ed629be8f418ab80ea
Aleksey Dombrovskiy <realkawul@gmail.com>
2017-11-21 09:39:45 +0200

    fixed

--- dombrovskiy_a-m4/script.js
+++ dombrovskiy_a-m4/script.js
@@ -10,36 +10,26 @@
       [str.slice(12, 23)],
       [str.slice(23)]
     ]
+    return keyboard;
     }
 
   addKeyboardLayout(alphabet);
-  console.log(keyboard);
-
 
   function getRandCharInRow(row) {
-    switch(row) {
-      case 0: let randomNumber0 = (Math.floor(Math.random() * ((((keyboard[0])[0])).length - 1 + 1)) + 0);
-              let randomElement0 = keyboard[0][0][randomNumber0];
-              console.log('random from row_0 = ',randomElement0);
-              break;
-      case 1: let randomNumber1 = (Math.floor(Math.random() * ((((keyboard[1])[0])).length - 1 + 1)) + 0);
-              let randomElement1 = keyboard[1][0][randomNumber1];
-              console.log('random from row_1 = ',randomElement1);
-              break;
-      case 2: let randomNumber2 = (Math.floor(Math.random() * ((((keyboard[2])[0])).length - 1 + 1)) + 0);
-              let randomElement2 = keyboard[2][0][randomNumber2];
-              console.log('random from row_2 = ',randomElement2);
-              break;
-    }
+    let lengthRow = (keyboard[row][0]).length;
+    let randowmNumberForRow = (Math.floor(Math.random() * lengthRow));
+    let randomCharFromRow = keyboard[row][0].charAt(randowmNumberForRow);
+    console.log(randomCharFromRow);
+    return randomCharFromRow;
   }
 
-  getRandCharInRow(0)
-
+  getRandCharInRow(2)
 
   function getRandCharInAlph (str) {
-    let randomNumberForAlphabet = (Math.floor(Math.random() * (str.length - 0 + 1) + 0));
+    let randomNumberForAlphabet = (Math.floor(Math.random() * str.length));
     let randomElementFromAlphabet = str.charAt(randomNumberForAlphabet);
     console.log('random from Alphabet = ', randomElementFromAlphabet);
+    return randomElementFromAlphabet;
 }
 
   getRandCharInAlph(alphabet);
