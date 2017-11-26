3437e542ffb17dde2213927a1bc9d34d05eef488
Nataliia Nechiporenko <nataliia.nechiporenko@gmail.com>
2017-11-27 00:05:21 +0200

    Finished js new_modules m4

--- nechyporenko_n-js_m4/js/script.js
+++ nechyporenko_n-js_m4/js/script.js
@@ -1,29 +1,64 @@
 'use strict';
 
+//Task 1
 const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-
-function addKeyboardLayout(alphabet) {
-let arrAlphabet = alphabet.split('');
 let keyboard = [];
 
-for(let i = 0, m = 0, n = 12; i < 3; i += 1) {
-  keyboard.push(arrAlphabet.slice(m, n));
-  m = n;
-  n = m + n - 1;
+function addKeyboardLayout(symbols) {
+    for (let i = 0, m = 0, n = 12; i < 3; i += 1) {
+        keyboard.push(symbols.split('').slice(m, n));
+        m = n;
+        n = m + n - 1;
+    };
+    return keyboard;
 }
 
-let word1 = keyboard[1][5] + keyboard[0][2] + keyboard[1][8] + keyboard[1][8] + keyboard[0][8];
+console.log(addKeyboardLayout(alphabet));
 
-let word2 = keyboard[1][6] + keyboard[1][0] + keyboard[2][3] + keyboard[1][0] +
-    keyboard[1][1] + keyboard[2][2] + keyboard[0][3] + keyboard[0][7] + keyboard[0][9] +
-    keyboard[0][4];
+//Task 2 & 3
+let buttons = {
+    btn_task2: (event) => {
+        let row = prompt('Enter number of row from 1 to 3:');
 
-let word3 = keyboard[0][4] + keyboard[0][3] + keyboard[1][0] + keyboard[0][7] +
-    keyboard[2][5] + keyboard[0][2] + keyboard[0][3];
+        if (row != '' && row > 0 && row <= 3) {
+            return getRandCharInRow(row);
+        } else {
+            alert('You entered wrong number of row. Please, enter number of row from 1 to 3');
+            buttons.btn_task2();
+        }
+    },
+    btn_task3: (event) => {
+        return getRandCharInAlph();
+    }
+};
 
-console.log(keyboard);
-console.log(`${word1} ${word2} ${word3}!`);
-console.log(`First word is ${word1}`);
-console.log(`Second word is ${word2}`);
-console.log(`Third word is ${word3}`);
-}
+
+function getRandIndex(max) {
+    let randIndex = Math.floor(Math.random() * (max + 1));
+    return randIndex;
+};
+
+function getRandCharInRow(row) {
+    row -= 1;
+    let charIndex = getRandIndex(keyboard[row].length);
+
+    console.log(`Random Char in the ${row+1} row is "${keyboard[row][charIndex]}"`);
+    return keyboard[row][charIndex];
+};
+
+function getRandCharInAlph() {
+    let allKeyboardArr = Array.from(keyboard.join("")).filter(n => n != ',');
+    let randInAll = getRandIndex(allKeyboardArr.length);
+
+    console.log(`Random character in all Alphabet is "${allKeyboardArr[randInAll]}"`);
+    return allKeyboardArr[randInAll];
+};
+
+document.body.addEventListener("click", function(event) {
+    let target = event.target;
+    for (let key in buttons) {
+        if (target.classList.contains(key)) {
+            buttons[key](event);
+        };
+    };
+});
