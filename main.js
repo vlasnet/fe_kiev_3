4571318d57386446d57faa63004bea60e9f67364
Miniaylo.S <po4tovuj@gmail.com>
2017-11-27 12:16:51 +0200

    fix functions destination

--- main.js
+++ main.js
@@ -19,9 +19,57 @@ let keyTrainer = {
             bottomRow: ['ячсмитьбю/']
         }
     },
-    numberToTrain: checkNumberToTrain,
-    trainChar: nextChar,
-    getRandChar: getRandCharInAlph,
+    checkNumberToTrain: function () {
+        let number = prompt('Укажите количество символов для тренировки');
+        if (!((number ^ 0) != number) && number > 0) {
+            this.numberToTrain = number;
+            console.log(keyTrainer.numberToTrain);
+            return this;
+        } else if (number === null) {
+            return;
+        } else {
+            alert('Введите количество символов для тренирови');
+            return checkNumberToTrain()
+        }
+    },
+    nextChar: function (a) {
+        let mistakes = 0;
+        alert(`Утановите расскладку клавиатуры ${this.currentLang}`);
+        for (let i = 0; i < a.length; i++) {
+            let symbol = prompt(`введите символ: ${a[i]}`);
+            if (symbol === null) {
+                let toBreak = confirm('Вы хотите прервать тренировку?');
+                if (toBreak === true) {
+                    return false;
+                } else {
+                    i--;
+                    continue;
+                }
+            } else if (symbol !== a[i]) {
+                mistakes++;
+            }
+        }
+        alert(`вы сделали ${mistakes} ошибок`);
+    },
+    getRandCharInAlph: function () {
+        let alphabet = '',
+            choosenLang = this.layouts[this.currentLang];
+
+        if (this.currentLang != '') {
+            for (let key in choosenLang) {
+                alphabet += choosenLang[key];
+            }
+
+            for (let i = 0; i < this.numberToTrain; i++) {
+                let rand = Math.floor(Math.random() * (alphabet.length - 1) + 1);
+                randArray.push(alphabet[rand]);
+                console.log(alphabet[rand]);
+            }
+            return this;
+        } else {
+            return false;
+        }
+    },
     currentLang: '',
     chooseLanguage: function() {
         switch (prompt('Выберите ваш язык en-0, ru-1, ua-2', '')) {
@@ -48,60 +96,6 @@ let keyTrainer = {
 
 let randArray = [];
 keyTrainer.chooseLanguage()
-              .numberToTrain()
-                  .getRandChar()
-                      .trainChar(randArray);
-
-function getRandCharInAlph() {
-    let alphabet = '',
-        choosenLang = this.layouts[this.currentLang];
-
-    if (this.currentLang != '') {
-        for (let key in choosenLang) {
-            alphabet += choosenLang[key];
-        }
-
-        for (let i = 0; i < this.numberToTrain; i++) {
-            let rand = Math.floor(Math.random() * (alphabet.length - 1) + 1);
-            randArray.push(alphabet[rand]);
-            console.log(alphabet[rand]);
-        }
-        return this;
-    } else {
-        return false;
-    }
-}
-
-function checkNumberToTrain() {
-    let number = prompt('Укажите количество символов для тренировки');
-    if (!((number ^ 0) != number) && number > 0) {
-        this.numberToTrain = number;
-        console.log(keyTrainer.numberToTrain);
-        return this;
-    } else if (number === null) {
-        return;
-    } else {
-        alert('Введите количество символов для тренирови');
-        return checkNumberToTrain()
-    }
-}
-
-function nextChar(a) {
-    let mistakes = 0;
-    alert(`Утановите расскладку клавиатуры ${this.currentLang}`);
-    for (let i = 0; i < a.length; i++) {
-        let symbol = prompt(`введите символ: ${a[i]}`);
-        if (symbol === null) {
-            let toBreak = confirm('Вы хотите прервать тренировку?');
-            if (toBreak === true) {
-                return false;
-            } else {
-                i--;
-                continue;
-            }
-        } else if (symbol !== a[i]) {
-            mistakes++;
-        }
-    }
-    alert(`вы сделали ${mistakes} ошибок`);
-}
+              .checkNumberToTrain()
+                  .getRandCharInAlph()
+                      .nextChar(randArray);
