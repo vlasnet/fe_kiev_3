bf13af43d2a76acaf250362c8e2d821237f90029
Sergey <C:\Users\Sergey\AppData\Roaming\The Bat!>
2017-12-22 21:44:17 +0200

    initial commit dz9

---
+++ vlasyuk_s_m-9/src/js/index.js
@@ -0,0 +1,79 @@
+/*jshint esversion: 6 */
+
+const lang = {
+    en: "qwertyuiop[]asdfghjkl;'zxcvbnm,./",
+    firstRow: ['do', 're', 'mi', 'fa', 'sol', 'la', 'la', 'sol', 'fa', 'mi', 're', 'do'],
+    secondRow: ['do', 're', 'mi', 'fa', 'sol', 'la', 'sol', 'fa', 'mi', 're', 'do'],
+    thirdRow: ['do', 're', 'mi', 'fa', 'sol', 'sol', 'fa', 'mi', 're', 'do'],
+};
+
+const html = document.querySelector('#keyboard-template').textContent.trim();
+const compiled = _.template(html);
+const result = compiled(lang);
+const keyboard = document.querySelector('.keyboard');
+
+keyboard.innerHTML = result;
+
+const playSound = note => {
+    const audio = document.querySelector(`audio[data-note=${note}]`);
+    audio.currentTime = 0;
+    audio.play();
+};
+
+const buttons = Array.from(document.querySelectorAll("button"));
+const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./ ".split("");
+
+const onKeydown = event => {
+    let pressedButton = event.which;
+    let pressedLetter;
+    let keyboardButton;
+    let sound;
+    let checkbox = document.querySelector("#slideThree");
+
+    if (pressedButton >= 65 && pressedButton <= 90) {
+        pressedLetter = String.fromCharCode(pressedButton).toLowerCase();
+    } else {
+        switch (pressedButton) {
+            case 219:
+                pressedLetter = "[";
+                break;
+            case 221:
+                pressedLetter = "]";
+                break;
+            case 186:
+                pressedLetter = ";";
+                break;
+            case 222:
+                pressedLetter = "'";
+                break;
+            case 188:
+                pressedLetter = ",";
+                break;
+            case 190:
+                pressedLetter = ".";
+                break;
+            case 191:
+                pressedLetter = "/";
+                break;
+            case 32:
+                pressedLetter = " ";
+                break;
+        }
+    }
+
+    if (keys.includes(pressedLetter)) {
+        keyboardButton = buttons[keys.indexOf(pressedLetter)];
+        sound = keyboardButton.getAttribute("data-note");
+        keyboardButton.classList.add("keyboard__btn--active");
+
+        if (checkbox.checked) {
+            playSound(sound);
+        }
+
+        setTimeout(() => {
+            keyboardButton.classList.remove("keyboard__btn--active");
+        }, 200);
+    }
+};
+
+window.addEventListener("keydown", onKeydown);
